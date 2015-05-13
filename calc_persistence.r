4cdb62a5f1292dbb4ee5ff946a75189d37da8f06
Alexander Robinson <robinson@fis.ucm.es>
2015-05-13 22:54:01 +0300

    Cleaned up.

--- calc_persistence.r
+++ calc_persistence.r
@@ -1,8 +1,11 @@
 
 # Load useful functions 
-library(myr)                # Only needed for plotting: myfigure function
+#library(myr)                # Only needed for plotting: myfigure function
 library(RNetCDF)
 source("functions_persistence.r")
+
+mdays = c(31,28,31,30,31,30,31,31,30,31,30,31)
+mfrac = cumsum(mdays) / sum(mdays)
 
 ## Load data ##
 if (TRUE) {
@@ -10,7 +13,7 @@ if (TRUE) {
     # Load the dataset 
     filename = "data/HadGHCND_TX_data3D.day1-365.1950-2011.nc"
 
-    nc = open.nc(filename)
+    nc  = open.nc(filename)
     dat = list()     
     dat$day  = var.get.nc(nc,"day")
     dat$year = var.get.nc(nc,"year")
@@ -28,37 +31,99 @@ if (TRUE) {
 if (TRUE) {
 
     ## User parameters 
-    smooth_nday = 91     # Must be odd 
-    smooth_nyr  =  1     # Must be odd 
+    nday = 91     # Must be odd 
+    nyr  =  5     # Must be odd 
 
-    # Calculate the running medians 
-    tas_sm = dat$tas*NA 
-    for (q in 1:length(dat$ID)) {
-        tas_sm[q,,] = calc_runmean(dat$tas[q,,],nday=smooth_nday)
+    # ntot = 1 
+    ntot = length(dat$ID)
 
+    # Calculate persistence information
+    cat("Calculating persistence... ")
+
+    per = list(trend=dat$tas*NA,ind=dat$tas*NA,dayp=dat$tas*NA,perp=dat$tas*NA,
+               dayn=dat$tas*NA,pern=dat$tas*NA)
+
+    # Check Western Europe 
+    qq = which(dat$lat >= 37 & dat$lat <= 55 & dat$lon >= -10 & dat$lon <= 10)
+
+    for (q in qq) {
+    # for (q in 1:ntot) {
+        tmp = calc_persistence(dat$tas[q,,],time=dat$time,nday=nday,nyr=nyr)
+        per$trend[q,,] = tmp$trend 
+        per$ind[q,,]   = tmp$ind 
+        per$dayp[q,,]  = tmp$dayp 
+        per$perp[q,,]  = tmp$perp 
+        per$dayn[q,,]  = tmp$dayn 
+        per$pern[q,,]  = tmp$pern 
     }
 
-    # Calculate persistence vector
-    per = dat$tas*0 
-    per[dat$tas >= tas_sm] =  1
-    per[dat$tas <  tas_sm] = -1 
-    per[is.na(per)] = 0              # To avoid NA values for cumsum function
+    cat("done.\n")
+
+    # Get binned persistence 
+    ss = list(c(12,1,2),c(3,4,5),c(6,7,8),c(9,10,11))
+    bins = seq(1949+0.9166667,2013,by=0.25)
+
+    tmpp = bin_persistence(per$dayp[qq,,],per$perp[qq,,],bins=bins)
+    tmpn = bin_persistence(per$dayn[qq,,],per$pern[qq,,],bins=bins)
 
 }
 
 fldr  = "plots"
 ptype = "pdf" 
 
-if (TRUE) {
 
-    # Make a simple test plot for a given point and year 
-    ip = 1 
-    k  = 60 
+# Plot persistence information 
+if (FALSE) {
 
-    xlim = range(dat$day)
+    # Check Western Europe 
+    qq = which(dat$lat >= 37 & dat$lat <= 55 & dat$lon >= -10 & dat$lon <= 10)
+
+    dayp = as.vector(per$dayp[qq,,])
+    perp = as.vector(per$perp[qq,,])
+    dayn = as.vector(per$dayn[qq,,])
+    pern = as.vector(per$pern[qq,,])
+
+    xlim = c(1949,2013)
+    ylim = range(perp,pern,na.rm=TRUE)
+
+    # myfigure(fldr,"per_WEUROPE",asp=1.0,pointsize=14)
+    par(mfrow=c(2,1))
+
+    par(plt=c(0.1,0.95,0.1,0.95))
+    plot(xlim,ylim,type='n',ann=FALSE)
+    points(dayp,perp,cex=0.8)
+    lines(tmpp$mids,tmpp$per,col=4,lwd=2)
+    abline(v=2003.5,col=2,lwd=1)
+    mtext(side=2,line=1.8,las=0,"Positive persistence (days)")
+    
+    par(new=TRUE,plt=c(0.22,0.35,0.7,0.9))
+    plot(c(-15,15),c(30,60),type="n",ann=FALSE,axes=FALSE)
+    map(add=TRUE,col="grey50")
+    points(dat$lon[qq],dat$lat[qq],pch=20,cex=0.5,col=2)
+
+    par(plt=c(0.1,0.95,0.1,0.95))
+    plot(xlim,ylim,type='n',ann=FALSE)
+    points(dayn,pern,cex=0.8)
+    lines(tmpn$mids,tmpn$per,col=4,lwd=2)
+    abline(v=2003.5,col=2,lwd=1)
+    mtext(side=2,line=1.8,las=0,"Negative persistence (days)")
+
+    # graphics.off()
+}
+
+
+
+
+if (FALSE) {
+
+    # Make a simple test plot
+    ip = qq[3] 
+    
+    xlim  = c(2001,2004)
+
     ylim1 = c(-12,18)
     ylim2 = c(-1,1)
-    ylim3 = c(-30,30)
+    ylim3 = c(-100,30)
 
     myfigure(fldr,"per_test",asp=1.0,pointsize=16)
     par(mfrow=c(3,1))
@@ -66,18 +131,18 @@ if (TRUE) {
 
     plot(xlim,ylim1,type="n",xlab="",ylab="Temperature anomaly (°C)")
     abline(h=0,col="grey40")
-    points(dat$day,dat$tas[ip,,k],col=1)
-    lines(dat$day,tas_sm[ip,,k],lwd=2,col=1)
+    points(dat$time,dat$tas[ip,,],col=1)
+    lines(dat$time,per$trend[ip,,],lwd=3,col=2)
 
     plot(xlim,ylim2,type="n",xlab="",ylab="Tendency")
     abline(h=0,col="grey40")
-    lines(dat$day,per[ip,,k],type="l",lwd=2,col=4,xlab="",ylab="Tendency")
+    lines(dat$time,per$ind[ip,,],type="l",lwd=2,col=4,xlab="",ylab="Tendency")
 
     plot(xlim,ylim3,type="n",xlab="",ylab="Persistence")
     abline(h=0,col="grey40")
-    lines(dat$day,cumsum(per[ip,,k]),lwd=2,col=4)
+    lines(dat$time,cumsum(per$ind[ip,,]),lwd=2,col=4)
 
-    mtext(side=1,line=1.5,cex=0.7,"Day")
+    # mtext(side=1,line=1.5,cex=0.7,"Year")
 
     graphics.off()
 }
--- functions_persistence.r
+++ functions_persistence.r
@@ -22,16 +22,156 @@ calc_runmean_year = function(ysm2D,nyr)
     # around each point in time
 
     trend = ysm2D*NA 
+    nday  = dim(ysm2D)[1] 
+    nyear = dim(ysm2D)[2]
 
-    # for ()
+    for (k in 1:nday) {
+        y = ysm2D[k,]
+        if (sum(is.na(y))<nyear) {
+            tmp = approx(c(1:length(y)),y,xout=c(1:length(y)),rule=2)$y 
+            trend[k,] = runmed(tmp,k=nyr)
+        }
+    }
 
     return(trend)
 }
 
-per_write = function(filename)
+calc_runmean_2D = function(y2D,nday,nyr)
+{   # Input 2D array: y[day,year],
+    # Calculate the running mean given a window of nyrs and ndays 
+    # around each point in time
+
+    # Wrap data so that there is a buffer on all sides 
+    nbuff1 = (nday-1)/2 
+    nbuff2 = (nyr-1)/2 
+
+    dims0 = dim(y2D)
+    dims  = dim(y2D)
+    dims[1] = dims[1]+2*nbuff1
+    dims[2] = dims[2]+2*nbuff2
+    
+    # Fill in buffered array 
+    y2Dex = array(NA,dim=dims)
+    y2Dex[1:nbuff1,(nbuff2+1):(dims[2]-nbuff2)] = y2D[(dims0[1]-nbuff1+1):dims0[1],]
+    y2Dex[(nbuff1+1):(dims[1]-nbuff1),(nbuff2+1):(dims[2]-nbuff2)] = y2D[,]
+    y2Dex[(dims[1]-nbuff1+1):dims[1],(nbuff2+1):(dims[2]-nbuff2)] = y2D[1:nbuff1,]
+
+    y2Dex[,1:nbuff2] = y2Dex[,((dims[2]-nbuff2+1):dims[2])-nbuff2]
+    y2Dex[,(dims[2]-nbuff2+1):dims[2]] = y2Dex[,(1:nbuff2)+nbuff2]
+
+
+    # Get indices of points describing our window
+
+    i0 = floor((nday-1)/2)
+    ii = c(-i0:i0)
+    k0 = floor((nyr-1)/2)
+    kk = c(-k0:k0)
+    
+    trendex = y2Dex*NA 
+
+    for (i in i0:(dim(y2Dex)[1]-i0)) {
+        for (k in k0:(dim(y2Dex)[2]-k0)) {
+            trendex[i,k] = mean(y2Dex[i+ii,k+kk],na.rm=TRUE)
+        }
+    }
+
+    # Cut off buffer 
+    trend = trendex[(nbuff1+1):(dims[1]-nbuff1),(nbuff2+1):(dims[2]-nbuff2)]
+
+    return(trend)
+}
+
+runmean <- function(x,n=31)
 {
+    n0 = floor((n-1)/2)
+    inds = c(-n0:n0)
+    xsm  = x*NA
+    for ( i in (n0+1):(length(x)-n0) ) xsm[i] = mean(x[i+inds],na.rm=TRUE)
+
+    return(xsm)
+}
+
+calc_persistence = function(y,time,trend=NULL,nday=91,nyr=5)
+{   # Given 2D inputs of y[nday,nyr] and the smooth trend[nday,nyr],
+    # calculate the persistence index (-1,0,1) and the persistence
+    # vector per[nevents,nyr] and the event_day[nevents,nyr]
+    # time should be a decimal-based vector 
+
+    if (length(y) != length(time)) {
+        cat("calc_persistence:: error: length of time vector does not match length of y.\n")
+        return(NULL)
+    }
+
+    # Calculate trend 
+    if (is.null(trend)) trend = calc_runmean_2D(y,nday=nday,nyr=nyr)
+
+    # Calculate persistence vector
+    per_ind = y*0 
+    per_ind[y >= trend]     =  1
+    per_ind[y <  trend]     = -1 
+    per_ind[is.na(per_ind)] = 0              # To avoid NA values
+
+    # Go through vector and calculate persistent events 
+    # Perform this step on a 1D vector to avoid artificial cutoffs 
+    # at day 1 and day 365 of the year 
+    per_ind1D = as.vector(per_ind) 
+    
+    per_pos  = rep(NA,length(time))   # [nevents,nyr] => max number of events in a year is 365
+    per_neg  = rep(NA,length(time))
+    mday_pos = rep(NA,length(time))
+    mday_neg = rep(NA,length(time))
+
+    pval  = per_ind1D[1]
+    nper  = 0
+    e_neg = 0 
+    e_pos = 0 
+
+    for (k in 1:length(time)) {
+
+        if (per_ind1D[k] == pval) {
+            nper = nper+1 
+        } else {
+            if (pval < 0) {
+                e_neg = e_neg + 1 
+                per_neg[e_neg]  = nper 
+                mday_neg[e_neg] = mean(time[(k-nper):k])
+            } else if (pval > 0) {
+                e_pos = e_pos + 1 
+                per_pos[e_pos]  = nper 
+                mday_pos[e_pos] = mean(time[(k-nper):k])
+            }
+
+            nper = 0 
+            pval = per_ind1D[k]
+        }
+
+    }
+
+    # Remove excess NA values from persistence event vectors 
+    ## TO DO ## 
 
 
+    return(list(trend=trend,ind=per_ind,perp=per_pos,dayp=mday_pos,pern=per_neg,dayn=mday_neg))
+
+}
+
+bin_persistence = function(days,per,bins)
+{
+    mids       = bins[1:(length(bins)-1)] + diff(bins)/2
+    per_binned = mids*NA 
+
+    for (k in 1:length(mids)) {
+        ij = which(days >= bins[k] & days < bins[k+1])
+        if (length(ij)>0) per_binned[k] = mean(per[ij],na.rm=TRUE)
+    }
+
+    return(list(mids=mids,per=per_binned))
+}
+
+per_write = function(filename)
+{   # Write a netcdf file with persistence information 
+
+    ## TO DO ##
 }
 
 
